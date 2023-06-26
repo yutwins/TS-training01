db1ce3f97cebc5fe852f9a1bf89b6f091f4db94c
yutwins <yutwo0717@gmail.com>
2023-06-26 17:44:40 +0900

    [tstraining]

---
+++ react_app/src/Todo.tsx
@@ -0,0 +1,7 @@
+import { TodoType } from "./types/todo";
+
+export const Todo = (props: Omit<TodoType, "id">) => {
+    const { title, userId, completed = false } = props;
+    const completeMark = completed ? "[完]" : "[未]";
+    return <p>{`${completeMark} ${title}(ユーザー:${userId})`}</p>
+};
